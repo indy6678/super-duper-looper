e331ece2cdb285fd732009119ef03d696934d2ec
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-11 21:16:38 -0700

    mod about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,13 @@
 import React from "react";
+import image from '../../logo512.png'
 
 function About() {
     return (
-        <section>
-            <h1>
+        <section className='my-10'>
+            <h1 id='about'>
                 About Me
             </h1>
+            <img src={image} className='' style={{ width: '50%'}} alt='cover' />
         </section>
     )
 }
